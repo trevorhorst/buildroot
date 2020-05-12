f1755d6bc3471d20fc2957843785896bba7c3582
Trevor Horst <trevor.horst@g3ti.net>
2020-05-11 22:13:56 -0400

    Improving ControlSoftware build file and custom buildroot package.

--- buildroot-2019.02.5/package/custom/ControlSoftware/ControlSoftware.mk
+++ buildroot-2019.02.5/package/custom/ControlSoftware/ControlSoftware.mk
@@ -4,8 +4,9 @@
 #
 #############################################################
 
+CONTROLSOFTWARE_BINARY	= control
 CONTROLSOFTWARE_VERSION = $(call qstrip,$(BR2_PACKAGE_CONTROLSOFTWARE_VERSION))
-CONTROLSOFTWARE_PROJECT = control
+CONTROLSOFTWARE_PROJECT = $(call qstrip,$(BR2_PACKAGE_CONTROLSOFTWARE_PROJECT))
 
 ifeq ($(BR2_PACKAGE_CONTROLSOFTWARE_CUSTOM_LOCAL),y)
 # Handler for the local package option
@@ -14,11 +15,14 @@ CONTROLSOFTWARE_SITE_METHOD = local
 else
 # Handler for the git option
 CONTROLSOFTWARE_SITE = https://github.com/trevorhorst/ControlSoftware/archive
+# CONTROLSOFTWARE_SITE = https://github.com/trevorhorst/ControlSoftware.git
 CONTROLSOFTWARE_SOURCE = $(CONTROLSOFTWARE_VERSION).tar.gz
 CONTROLSOFTWARE_SITE_METHOD = git 
 endif
 
 CONTROLSOFTWARE_DEPENDENCIES = readline libmicrohttpd libcurl
+# CONTROLSOFTWARE_CONF_ENV = source $(@D)/hardware/$(CONTROLSOFTWARE_PROJECT)/settings.sh
+CONTROLSOFTWARE_CONF_ENV = CONTROL_PROJECT=$(CONTROLSOFTWARE_PROJECT)
 
 # Some hooks that we may want to use in the future
 # define CONTROLSOFTWARE_CONFIGURE_CMDS
@@ -30,7 +34,7 @@ define CONTROLSOFTWARE_INSTALL_TARGET_CMDS
     mkdir -p $(TARGET_DIR)/usr/local/bin
     echo "Moving daemon to the images directory"
     # cp -a $(@D)/app/$(CONTROLSOFTWARE_PROJECT)d $(TARGET_DIR)/../images/
-    cp -a $(@D)/app/$(CONTROLSOFTWARE_PROJECT)d $(TARGET_DIR)/usr/local/bin
+    cp -a $(@D)/app/$(CONTROLSOFTWARE_BINARY)d $(TARGET_DIR)/usr/local/bin
 endef
 
 $(eval $(cmake-package))
